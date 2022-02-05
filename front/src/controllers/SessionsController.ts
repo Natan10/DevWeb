e0188499ca7fff24600a9e90ca5a37f15f9fa8c2
Natan <natan-moreira1@hotmail.com>
2022-02-05 17:09:48 -0300

    feat: refactor login and signUp to use backend

--- front/src/controllers/SessionsController.ts
+++ front/src/controllers/SessionsController.ts
@@ -1,42 +1,24 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-import { User } from "../models/user";
-import { sign } from "jsonwebtoken";
 import logger from "../logger/logger";
+import api from "../../config/api";
 
 class SessionsController {
   async handler(req: Request, res: Response) {
     const { email, password } = req.body;
-    const prisma = new PrismaClient();
 
     try {
-      const user = await prisma.user.findFirst({
-        where: { email },
+      const { data } = await api.post("/login", {
+        email,
+        password,
       });
 
-      if (!user) {
-        logger.info("GET /entrar - User não encontrado");
-        return res.status(404).json({ message: "Usuário não encontrado!" });
-      }
-
-      if (await User.comparePassword(password, user.password)) {
-        const token = sign(
-          {
-            id: user.id,
-          },
-          process.env.JWT_SECRET?.toString() || ""
-        );
-
-        res.cookie("userToken", token, {
+      if (data.token) {
+        res.cookie("userToken", data.token, {
           httpOnly: true,
           expires: new Date(2000000 + Date.now()),
         });
-
-        return res.redirect("/admin");
-      } else {
-        logger.info("GET /entrar - User password incorreto");
-        return res.status(401).json("Password incorreto!");
       }
+      return res.redirect("/admin");
     } catch (err) {
       logger.error(err);
       return res.status(500).json({ error: "Erro ao logar!" });
--- front/src/controllers/UsersController.ts
+++ front/src/controllers/UsersController.ts
@@ -2,39 +2,22 @@ import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { User } from "../models/user";
 import logger from "../logger/logger";
+import api from "../../config/api";
 
 class UsersController {
   async create(req: Request, res: Response) {
-    const prisma = new PrismaClient();
     const { nome, email, password } = req.body;
 
     try {
-      const userExist = await prisma.user.findFirst({
-        where: {
-          email,
-        },
+      await api.post("/user", {
+        nome,
+        email,
+        password,
       });
-
-      if (userExist) {
-        logger.error(`POST /cadastrar - User já existe`);
-        return res.status(400).json({ message: "User já existe!" });
-      }
-
-      const user = new User(nome, email, password);
-      await prisma.user.create({
-        data: {
-          nome: user.Nome,
-          email: user.Email,
-          password: await User.hashPassword(password),
-        },
-      });
-
-      logger.info(`POST /cadastrar - User criado`);
 
       res.setHeader("Content-Type", "text/html");
       return res.redirect("/entrar");
-    } catch (err) {
-      logger.error(err);
+    } catch (err: any) {
       return res.status(400).json({ error: "Erro ao criar usuário" });
     }
   }
