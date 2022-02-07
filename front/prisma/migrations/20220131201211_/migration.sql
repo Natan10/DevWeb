3d5ddde7438d9b260a7008c09dd5e81a9287da7a
Natan <natan-moreira1@hotmail.com>
2022-02-07 00:37:42 -0300

    chore: delete prisma to front project

--- front/prisma/migrations/20220131201211_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Promotion" DROP CONSTRAINT "Promotion_userId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Promotion" ADD CONSTRAINT "Promotion_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
